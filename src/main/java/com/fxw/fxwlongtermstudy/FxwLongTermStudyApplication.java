138e969ef409f9daad76df1735ad2fd728f59b4f
fanxinweizuiniubi <fanxinwei76@gmail.com>
2020-11-10 14:34:44 +0800

    git add . 测试

---
+++ src/main/java/com/fxw/fxwlongtermstudy/FxwLongTermStudyApplication.java
@@ -0,0 +1,13 @@
+package com.fxw.fxwlongtermstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxwLongTermStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxwLongTermStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fxw/fxwlongtermstudy/FxwLongTermStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fxw.fxwlongtermstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxwLongTermStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
